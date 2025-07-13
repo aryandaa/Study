72e0a339e01a076f9891bcb4d237622523e90711
aryandaa <114203619+aryandaa@users.noreply.github.com>
2025-07-13 20:24:21 +0800

    .

---
+++ Language/Php/Syntak Dasar/15.Math.php
@@ -0,0 +1,18 @@
+<?php
+/*PHP memiliki serangkaian fungsi matematika yang melakukan tugas matematika pada angka.*/
+
+echo(pi()); //Mengembalikan nilai phi "3.1415926535898"
+echo "<br>";
+echo(min(0, 150, 30, 20, -8, -200)); //mencari nilai paling kecil
+echo "<br>";
+echo(max(0, 150, 30, 20, -8, -200)); //mencari nilai paling besar
+echo "<br>";
+echo(abs(-6.7)); //mengemnbalikan nilai absolute(positive) yang bernilai angka/number
+echo "<br>";
+echo(sqrt(64)); //mengembalikan akar
+echo "<br>";
+echo(round(0.60)); //pembulatan
+echo "<br>";
+echo(rand(1, 10)); //random number dengan antara .. sampai ..
+
+?>
