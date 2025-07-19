04a6e7e588d97c716758d30ad40a31f9e3a1d5c2
aryandaa <114203619+aryandaa@users.noreply.github.com>
2025-07-19 17:59:16 +0800

    Update kalkulator.php

--- Language/Php/Latihan - Basic/kalkulator.php
+++ Language/Php/Latihan - Basic/kalkulator.php
@@ -1,11 +1,8 @@
 <?php
 //membuat program kalkulator sederhana dari inputan user
 
-$angka1 = readline('input angka pertama: '); //mengambil inputan user
-$ToInt = (Int)$angka1; //diubah dulu ke integer agar bisa dijumlahkan
-
+$angka1 = readline('input angka pertama: ');
 $angka2 = readline('input angka kedua: '); 
-$ToInt2 = (Int)$angka2;
 
 $aritmatika = readline('masukan perhitungan: ');
 switch ($aritmatika) {
